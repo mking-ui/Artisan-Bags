7b7b330c2286abdda8fdcb8e4a0bfea25909ec75
Micah Enoch <enochmicah3@.com>
2025-09-20 03:28:58 +1300

    cartItems Logics

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,4 +1,5 @@
 import Product from "@/models/Product";
+import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -18,8 +19,25 @@ export async function POST (request){
             return await acc + product.offerPrice *item.quantity
         },0)
 
+        await inngest.send({
+            name:"order/created",
+            data:{
+                userId,
+                address,
+                items,
+                amount: amount + Math.floor(amount *0.02),
+                date:Date.now()
+            }
+        })
+        //clear user cart
+        const user = await User.findById(userId)
+        user.cartItems={}
+        await user.save()
+return NextResponse.json ({success: true, message:"Order Placed"})
         
     } catch (error) {
-        
+        console.log(error)
+        return NextResponse.json ({success: false, message:error.message})
+ 
     }
 }
